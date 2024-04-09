037507e6fa973c405a90d09082bc69862fb11fe1
SamAxler <101593945+SamAxler@users.noreply.github.com>
2024-04-09 10:01:08 -0400

    Update FinalProject.py

--- FinalProject.py
+++ FinalProject.py
@@ -7,18 +7,30 @@ import webbrowser
 
 
 coords = [
-    [-75.11176, 40.11733],
-    [-75.11933,40.11894],
-    [-75.11631,40.12466]
+    [40.11733,-75.11176],
+    [40.11894,-75.11933],
+    [40.12466,-75.11631]
 
 ]
-start = [-75.11176, 40.11733]
+
+line_points = [
+    [40.11733,-75.11176],
+    [40.11894,-75.11933],
+    [40.12466,-75.11631]
+              ]
+
+
+start = [40.11733,-75.11176]
 
 # visualize the points on a map
-m = folium.Map(location=list(reversed([-75.11176, 40.11733])), tiles="cartodbpositron", zoom_start=12)
+m = folium.Map(location=list(start), tiles="cartodbpositron", zoom_start=12)
 for coord in coords:
-    folium.Marker(location=list(reversed(coord))).add_to(m)
-folium.Marker(location=list(reversed(start)), icon=folium.Icon(color="red")).add_to(m)
+    folium.Marker(location=list(coord)).add_to(m)
+folium.Marker(location=list(start), icon=folium.Icon(color="red")).add_to(m)
+
+lines_group = folium.FeatureGroup(name="Lines").add_to(m)
+lines_group.add_child(folium.PolyLine(locations=line_points, weight=3,color = 'blue'))
+
 m
 m.save('aee.html')
 
@@ -26,4 +38,4 @@ m.save('aee.html')
 def auto_open(url):
     webbrowser.open(url, new=2)
 
-auto_open('aee.html')
+auto_open('aee.html
