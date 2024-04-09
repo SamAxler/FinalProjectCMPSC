4110547d27e4815bbb0ded2b827e657b296a2740
SamAxler <101593945+SamAxler@users.noreply.github.com>
2024-04-09 09:34:04 -0400

    Update FinalProject.py

--- FinalProject.py
+++ FinalProject.py
@@ -1,11 +1,15 @@
-
 import folium
+import subprocess
+from IPython.core.display import display, HTML
 import openrouteservice as ors
 import math
+import webbrowser
+
 
 coords = [
     [-75.11176, 40.11733],
-    [-75.11933,40.11894]
+    [-75.11933,40.11894],
+    [-75.11631,40.12466]
 
 ]
 start = [-75.11176, 40.11733]
@@ -16,3 +20,10 @@ for coord in coords:
     folium.Marker(location=list(reversed(coord))).add_to(m)
 folium.Marker(location=list(reversed(start)), icon=folium.Icon(color="red")).add_to(m)
 m
+m.save('aee.html')
+
+
+def auto_open(url):
+    webbrowser.open(url, new=2)
+
+auto_open('aee.html')
