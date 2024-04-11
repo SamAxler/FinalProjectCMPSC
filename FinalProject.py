a7ef30a9d0fc367e680ead57bd55b431a09d5fd5
SamAxler <101593945+SamAxler@users.noreply.github.com>
2024-04-11 09:38:39 -0400

    Update FinalProject.py

--- FinalProject.py
+++ FinalProject.py
@@ -1,5 +1,3 @@
-#5b3ce3597851110001cf62483d1d56a31e134c83851721865a6ac5c4
-
 import folium
 import subprocess
 from IPython.core.display import display, HTML
@@ -7,38 +5,32 @@ import openrouteservice as ors
 import math
 import webbrowser
 
-coords = [
-    [40.11733,-75.11176],
-    [40.11894,-75.11933],
-    [40.12466,-75.11631]
+client = ors.Client(key='5b3ce3597851110001cf62483d1d56a31e134c83851721865a6ac5c4')
 
+linepoints2 = [
+    [-75.11176,40.11733],
+    [-75.11933,40.11894],
+    [-75.11631,40.12466],
+    [-74.8794,40.2248]
 ]
-
-line_points = [
-    [40.11733,-75.11176],
-    [40.11894,-75.11933],
-    [40.12466,-75.11631]
-              ]
-
 
 start = [40.11733,-75.11176]
 
 # visualize the points on a map
 m = folium.Map(location=list(start), tiles="cartodbpositron", zoom_start=12)
-for coord in coords:
-    folium.Marker(location=list(coord)).add_to(m)
+for coord in linepoints2:
+    folium.Marker(location=list(reversed(coord))).add_to(m)
 folium.Marker(location=list(start), icon=folium.Icon(color="red")).add_to(m)
 
 lines_group = folium.FeatureGroup(name="Lines").add_to(m)
-lines_group.add_child(folium.PolyLine(locations=line_points, weight=3,color = 'blue'))
 
+route = client.directions(coordinates=linepoints2,profile='driving-car',format='geojson')
+folium.PolyLine(locations=[list(reversed(coord)) for coord in route['features'][0]['geometry']['coordinates']],color = "blue").add_to(m)
 m
 m.save('aee.html')
-
-
 def auto_open(url):
     webbrowser.open(url, new=2)
 
 
-auto_open('aee.html')
 
+auto_open('aee.html')
