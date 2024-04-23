755b28bc71e7ee5cc4dd54452371100b4301ae16
Ab-Salem <162937389+Ab-Salem@users.noreply.github.com>
2024-04-23 16:26:11 -0400

    Update osm.py

--- osm.py
+++ osm.py
@@ -1,88 +1,65 @@
-from math import radians, cos, sin, asin, sqrt
 import copy
-import xml.sax # parse osm file
-from pathlib import Path # manage cached tiles
-
-from WeightedGraph import *
-
-
-def haversine(lon1, lat1, lon2, lat2, unit_m = True):
-    """
-    Calculate the great circle distance between two points
-    on the earth (specified in decimal degrees)
-    default unit : km
-    """
-    # convert decimal degrees to radians
-    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
-
-    # haversine formula
-    dlon = lon2 - lon1
-    dlat = lat2 - lat1
-    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
-    c = 2 * asin(sqrt(a))
-    r = 6371 # Radius of earth in kilometers. Use 3956 for miles
-    if (unit_m):
-        r *= 1000
-    return c * r
+import urllib.request
+import xml.sax 
+from pathlib import Path 
 
 
-def read_osm(filename_or_stream, only_roads=True):
-    """Read graph in OSM format from file specified by name or by stream object.
-    Parameters
-    ----------
-    filename_or_stream : filename or stream object
-    Returns
-    -------
-    G : Graph
-    Examples
-    --------
-    >>> G=nx.read_osm(nx.download_osm(-122.33,47.60,-122.31,47.61))
-    >>> import matplotlib.pyplot as plt
-    >>> plt.plot([G.node[n]['lat']for n in G], [G.node[n]['lon'] for n in G], 'o', color='k')
-    >>> plt.show()
-    """
-    osm = OSM(filename_or_stream)
-    G = WeightedGraph()
-    # clear graph 
-    # G.clear()
+def download_osm(left=-73.4244, bottom=45.4302, right=-73.4010, top=45.4466, proxy=False, proxyHost="10.0.4.2", proxyPort="3128", cache=False, cacheTempDir="/tmp/tmpOSM/", verbose=True):
+    """ Return a filehandle to the downloaded data from osm api."""
+    if cache:
+        # cached tile filename
+        cachedTileFilename = "osm_map_{:.8f}_{:.8f}_{:.8f}_{:.8f}.map".format(left, bottom, right, top)
 
-    ## Add ways
-    for w in osm.ways.values():
-        if only_roads and 'highway' not in w.tags:
-            continue
+        if verbose:
+            print("Cached tile filename :", cachedTileFilename)
 
-        if ('oneway' in w.tags):
-            if (w.tags['oneway'] == 'yes'):
-                # ONLY ONE DIRECTION
-                G.add_path(w.nds, id=w.id)
-            else:
-                # BOTH DIRECTION
-                G.add_path(w.nds, id=w.id)
-                G.add_path(w.nds[::-1], id=w.id)
-        else:
-            # BOTH DIRECTION
-            G.add_path(w.nds, id=w.id)
-            G.add_path(w.nds[::-1], id=w.id)
+        cacheTempDir = Path(cacheTempDir)
+        cacheTempDir.mkdir(parents=True, exist_ok=True)  # Create cache path if not exists
 
-     ## Complete the used nodes' information
-    for n_id in G.nodes_iter():
-        if n_id in osm.nodes:
-            n = osm.nodes[n_id]
-            G.node[n_id]['lat'] = n.lat
-            G.node[n_id]['lon'] = n.lon
-            G.node[n_id]['id'] = n.id
+        osmFile = Path(cacheTempDir / cachedTileFilename).resolve()  # Replace the relative cache folder path to absolute path
 
-    ## Estimate the length of each way
-    for u,v,d in G.edges_iter(data=True):
-        distance = haversine(G.node(u)['lon'], G.node(u)['lat'], G.node(v)['lon'], G.node(v)['lat'], unit_m=True)
- # Give a realistic distance estimation (neither EPSG nor projection nor reference system are specified)
+        if osmFile.is_file():
+            # download from the cache folder
+            if verbose:
+                print("Tile loaded from the cache folder.")
 
-        G.add_weighted_edges_from([( u, v, distance)], weight='length')
+            with open(osmFile, mode='r') as f:
+                content = f.read()
+            return content
 
-    return G
+    if proxy:
+        # configure the urllib request with the proxy
+        proxy_handler = urllib.request.ProxyHandler({'https': 'https://' + proxyHost + ":" + proxyPort, 'http': 'http://' + proxyHost + ":" + proxyPort})
+        opener = urllib.request.build_opener(proxy_handler)
+        urllib.request.install_opener(opener)
+
+    request = "http://api.openstreetmap.org/api/0.6/map?bbox=%f,%f,%f,%f" % (left, bottom, right, top)
+
+    if verbose:
+        print("Download the tile from osm web api ... in progress")
+        print("Request :", request)
+
+    fp = urllib.request.urlopen(request)
+    content = fp.read().decode('utf-8')
+
+    if verbose:
+        print("OSM Tile downloaded")
+
+    if cache:
+        if verbose:
+            print("Write osm tile in the cache")
+
+        with open(osmFile, 'w') as f:
+            f.write(content)
+
+        if osmFile.is_file():
+            if verbose:
+                print("OSM tile written in the cache")
+
+    return content
 
 
-class Node:
+class Node(object):
     def __init__(self, id, lon, lat):
         self.id = id
         self.lon = lon
@@ -93,7 +70,7 @@ class Node:
         return "Node (id : %s) lon : %s, lat : %s "%(self.id, self.lon, self.lat)
 
 
-class Way:
+class Way(object):
     def __init__(self, id, osm):
         self.osm = osm
         self.id = id
@@ -117,21 +94,23 @@ class Way:
 
         # create a way object for each node-array slice
         ret = []
-        i=0
+        i = 0
         for slice in slices:
-            littleway = copy.copy( self )
-            littleway.id += "-%d"%i
+            littleway = copy.copy(self)
+            littleway.id += "-%d" % i
             littleway.nds = slice
-            ret.append( littleway )
+            ret.append(littleway)
             i += 1
 
         return ret
 
 
+class OSM(object):
+    def __init__(self, osm_xml_data, is_xml_string=True):
+        """ File can be either a filename or stream/file object.
 
-class OSM:
-    def __init__(self, filename_or_stream):
-        """ File can be either a filename or stream/file object."""
+        set `is_xml_string=False` if osm_xml_data is a filename or a file stream.
+        """
         nodes = {}
         ways = {}
 
@@ -139,7 +118,7 @@ class OSM:
 
         class OSMHandler(xml.sax.ContentHandler):
             @classmethod
-            def setDocumentLocator(self,loc):
+            def setDocumentLocator(self, loc):
                 pass
 
             @classmethod
@@ -152,41 +131,45 @@ class OSM:
 
             @classmethod
             def startElement(self, name, attrs):
-                if name=='node':
+                if name == 'node':
                     self.currElem = Node(attrs['id'], float(attrs['lon']), float(attrs['lat']))
-                elif name=='way':
+                elif name == 'way':
                     self.currElem = Way(attrs['id'], superself)
-                elif name=='tag':
+                elif name == 'tag':
                     self.currElem.tags[attrs['k']] = attrs['v']
-                elif name=='nd':
-                    self.currElem.nds.append( attrs['ref'] )
+                elif name == 'nd':
+                    self.currElem.nds.append(attrs['ref'])
 
             @classmethod
-            def endElement(self,name):
-                if name=='node':
+            def endElement(self, name):
+                if name == 'node':
                     nodes[self.currElem.id] = self.currElem
-                elif name=='way':
+                elif name == 'way':
                     ways[self.currElem.id] = self.currElem
 
             @classmethod
             def characters(self, chars):
                 pass
 
-        xml.sax.parse(filename_or_stream, OSMHandler)
+        if is_xml_string:
+            xml.sax.parseString(osm_xml_data, OSMHandler)
+        else:
+            with open(osm_xml_data, mode='r') as f:
+                xml.sax.parse(f, OSMHandler)
 
         self.nodes = nodes
         self.ways = ways
 
-        #count times each node is used
-        node_histogram = dict.fromkeys( self.nodes.keys(), 0 )
+        # count times each node is used
+        node_histogram = dict.fromkeys(self.nodes.keys(), 0)
         for way in self.ways.values():
-            if len(way.nds) < 2:       #if a way has only one node, delete it out of the osm collection
+            if len(way.nds) < 2:  # if a way has only one node, delete it out of the osm collection
                 del self.ways[way.id]
             else:
                 for node in way.nds:
                     node_histogram[node] += 1
 
-        #use that histogram to split all ways, replacing the member set of ways
+        # use that histogram to split all ways, replacing the member set of ways
         new_ways = {}
         for id, way in self.ways.items():
             split_ways = way.split(node_histogram)
